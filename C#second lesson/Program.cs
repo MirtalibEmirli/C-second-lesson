72b187780b62e699cbb3826ef8d3c5541f477086
MirtalibEmirli <azad.sdk.85@icloud.com>
2024-02-15 15:06:18 +0400

    Add project files.

---
+++ C#second lesson/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine(@"        
+                                ============
+                                 Calculator     ");
+Console.ForegroundColor = ConsoleColor.White;
+
+
+
+Console.ReadKey();
+
+
+/*QEYDLER  */
+
+/*
+ Consolda yazdigmz ve consoldan alnan her sey stringdir
+Butun data tpler in en esasi object classi dir butun type lar ondan torenib
+Ve object classindan diger butun field ler Tostring methodu kecib buttun her sey stringe cevrle blir
+
+ 
+ 
+ 
+ 
+ 
+ 
+ */
